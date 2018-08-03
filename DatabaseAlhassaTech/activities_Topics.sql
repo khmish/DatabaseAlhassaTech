2d6fbeb0be3f62ee91f5131976499ef9ed5800d3
Hassan <hkh_2006@hotmail.com>
2018-08-03 18:07:04 +0300

    Revert "Merge branch 'master' of https://github.com/khmish/DatabaseAlhassaTech"
    This reverts commit 4d5c86aaaa47c523923dd3441e5a57797ccfbdf6.

---
+++ DatabaseAlhassaTech/activities_Topics.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[activities_Topics]
+(
+	[ID] INT NOT NULL PRIMARY KEY, 
+    [user_id] INT NOT NULL, 
+    [team_id] INT NOT NULL, 
+    [title] VARCHAR(50) NULL, 
+    [description] VARCHAR(500) NULL, 
+    [activity_type] VARCHAR(50) NULL, 
+    [topic_img] IMAGE NULL, 
+    [content] TEXT NULL, 
+    [date_submit] DATE NOT NULL, 
+    [view_count] INT NULL, 
+    [like_count] INT NULL, 
+    [comm_count] INT NULL, 
+    [start_date] DATE NOT NULL, 
+    [number_of_days] INT NOT NULL, 
+    [start_time] DATE NOT NULL, 
+    [number_of_hours] INT NOT NULL, 
+    [activity_location] VARCHAR(150) NOT NULL
+)
---
+++ DatabaseAlhassaTech/notiﬁcations.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[notiﬁcations]
+(
+	[id] INT NOT NULL PRIMARY KEY, 
+    [userId] INT NOT NULL, 
+    [senderId] INT NOT NULL, 
+    [topicId] INT NOT NULL, 
+    [title] VARCHAR(50) NOT NULL, 
+    [date] DATE NOT NULL, 
+    [read] TINYINT NOT NULL, 
+    [link] VARCHAR(MAX) NOT NULL 
+)
