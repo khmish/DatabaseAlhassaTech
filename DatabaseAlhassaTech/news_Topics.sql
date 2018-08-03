961fb4fad801194e7b357bc1d0f0d5254b984428
Hassan <hkh_2006@hotmail.com>
2018-08-03 18:06:36 +0300

    fu!* log

--- DatabaseAlhassaTech/news_Topics.sql
+++ DatabaseAlhassaTech/news_Topics.sql
@@ -1,4 +1,13 @@
 ﻿CREATE TABLE [dbo].[news_Topics]
 (
-	[ID] INT NOT NULL PRIMARY KEY
+	[ID] INT NOT NULL PRIMARY KEY, 
+    [user_id] INT NULL, 
+    [title] VARCHAR(50) NULL, 
+    [description] VARCHAR(MAX) NULL, 
+    [topic_img] IMAGE NULL, 
+    [content] TEXT NULL, 
+    [date_submit] DATE NULL, 
+    [view_count] INT NULL, 
+    [like_count] INT NULL, 
+    [comm_count] INT NULL
 )
