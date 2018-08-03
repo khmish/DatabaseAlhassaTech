c3a9d8b983c683a61fa81ad2f79321ddec556be0
hassan1413 <Hassan14131@gmail.com>
2018-08-03 18:00:36 +0300

    hi

---
+++ DatabaseAlhassaTech/admin_notif.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[admin_notif]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [title] VARCHAR(50) NULL, 
+    [type] VARCHAR(50) NULL, 
+    [date] DATE NULL, 
+    [is_solved] TINYINT NULL, 
+    [date_solved] DATE NULL, 
+    [comments] text NULL, 
+    [link] VARCHAR(max) NULL
+)
